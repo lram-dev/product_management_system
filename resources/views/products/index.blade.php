e3559efc513e91890dbfa74a14e500a3dc8e3175
Ricardo Acosta <ricardo.acosta.meza.lram@gmail.com>
2026-03-07 01:30:26 -0600

    Cree un nuevo producto y actualice la vista de índice para mostrar los nuevos campos. agregue estilos de twind a la vista de índice para mejorar la apariencia de la tabla y el botón de creación. ejecute por terminal run dev para compilar los activos de twind y ver los cambios en el navegador.

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,1 +1,61 @@
-<h1>Hola mundo</h1>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Lista de Productos') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-6 lg:p-8">
+                <div class="bg-white bg-gray-800 border-b border-gray-200 border-gray-700">
+
+                    <div class="mb-4">
+                        <a href="{{ route('products.create') }}"
+                            class="bg-cyan-500 dark:bg-cyan-700 hover:bg-cyan-600 dark:hover:bg-cyan-800 text-white font-bold py-2 px-4 rounded">
+                            Create
+                        </a>
+                    </div>
+
+                    <table class="table-auto w-full">
+                        <thead>
+                            <tr>
+                                <th class="px-4 py-2 text-gray-900 text-dark text-center">ID</th>
+                                <th class="px-4 py-2 text-gray-900 text-dark text-center">Modelo</th>
+                                <th class="px-4 py-2 text-gray-900 text-dark text-center">Proveedor</th>
+                                <th class="px-4 py-2 text-gray-900 text-dark text-center">Galeria</th>
+                                <th class="px-4 py-2 text-gray-900 text-dark text-center">Desperdicio</th>
+                                <th class="px-4 py-2 text-gray-900 text-dark text-center">Costo</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($products as $product)
+                            <tr>
+                                <td class="border px-4 py-2 text-gray-900 dark:text-white text-center">
+                                    {{ $product->id }}
+                                </td>
+                                <td class="border px-4 py-2 text-gray-900 dark:text-white text-center">
+                                    {{ $product->modelo }}
+                                </td>
+                                <td class="border px-4 py-2 text-gray-900 dark:text-white text-center">
+                                    {{ $product->proveedor }}
+                                </td>
+                                <td class="border px-4 py-2 text-gray-900 dark:text-white text-center">
+                                    {{ $product->galeria }}
+                                </td>
+                                <td class="border px-4 py-2 text-gray-900 dark:text-white text-center">
+                                    {{ $product->desperdicio }}
+                                </td>
+                                <td class="border px-4 py-2 text-gray-900 dark:text-white text-center">
+                                    {{ $product->costo }}
+                                </td>
+
+                            </tr>
+
+                            @endforeach
+                        </tbody>
+                </div>
+            </div>
+        </div>
+</x-app-layout>
